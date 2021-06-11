20d8182f39a24efc62a055ccdea6f5345880028b
Jacinth David <jacinthdavid@gmail.com>
2021-06-10 22:36:01 -0400

    add logging

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from dotenv import find_dotenv, load_dotenv
 import pymysql
 import crypto_api
 import crypto_sql
+import logging
 
 load_dotenv(find_dotenv(raise_error_if_not_found=True))
 
@@ -14,6 +15,11 @@ load_dotenv(find_dotenv(raise_error_if_not_found=True))
 # os.getenv("DB_HOST")
 
 # Start Here
+logging.basicConfig(filename='storage/logs/app.log', level=logging.INFO)
+# https://docs.python.org/3/howto/logging-cookbook.html
+console_ch = logging.StreamHandler()
+console_ch.setLevel(logging.INFO)
+logging.getLogger('').addHandler(console_ch)
 
 
 def connect_to_mysql():
@@ -52,24 +58,29 @@ if __name__ == "__main__":
                     loss_gain = ((curr_price - buy_price) / buy_price) * 100
                     purchases.append([symbol, buy_price, curr_price, loss_gain])
 
+                # edge case: haven't encoded business rules when coin information changes
                 if symbol not in existing_coins_set:
                     new_coins.append([symbol, name])
 
             # edge case: should check for length of symbol and price because might exceed database column length
+            logging.info(f'coin prices obtained from api: {coin_prices}')
             cursor.executemany(crypto_sql.insert_coin_prices, coin_prices)
 
             # enter new coins into database
             if new_coins:
                 # edge case: should check for length of symbol and name because might exceed database column length
+                logging.info(f'new coins encountered: {new_coins}')
                 cursor.executemany(crypto_sql.insert_coins, new_coins)
 
             # enter purchases into portfolio
             if purchases:
+                logging.info(f'coins purchased: {purchases}')
                 cursor.executemany(crypto_sql.insert_portfolio, purchases)
 
         # connection is not autocommit by default. So commit to save changes.
         connection.commit()
 
+        '''
         # for checking result
         with connection.cursor() as cursor:
             cursor.execute(crypto_sql.existing_coins)
@@ -84,3 +95,4 @@ if __name__ == "__main__":
             print(result_coins)
             print(result_coin_prices)
             print(result_portfolio)
+        '''
